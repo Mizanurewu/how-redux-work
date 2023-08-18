7e4d7a2673215fe2d1967cc3c9f404f35b13f5d7
Mizanur Rahman <mizanurewu@gmail.com>
2023-08-18 21:09:03 +0600

    1.7 payload and action creator

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
-
 // select dom elements
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+//action identifier
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+//action creator
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
+
 // initial state
 const initialState = {
-    value: 0,
+  value: 0,
 };
 
 // create reducer function
 function counterReducer(state = initialState, action) {
-    if (action.type === "increment") {
-        return {
-            ...state,
-            value: state.value + 1,
-        };
-    } else if (action.type === "decrement") {
-        return {
-            ...state,
-            value: state.value - 1,
-        };
-    } else {
-        return state;
-    }
+  if (action.type === INCREMENT) {
+    return {
+      ...state,
+      value: state.value + action.payload,
+    };
+  } else if (action.type === DECREMENT) {
+    return {
+      ...state,
+      value: state.value - action.payload,
+    };
+  } else {
+    return state;
+  }
 }
 
 // create store
 const store = Redux.createStore(counterReducer);
 
 const render = () => {
-    const state = store.getState();
-    counterEl.innerText = state.value.toString();
+  const state = store.getState();
+  counterEl.innerText = state.value.toString();
 };
 
 // update UI initially
@@ -41,13 +59,9 @@ store.subscribe(render);
 
 // button click listeners
 incrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment",
-    });
+  store.dispatch(increment(5));
 });
 
 decrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "decrement",
-    });
+  store.dispatch(decrement(3));
 });
