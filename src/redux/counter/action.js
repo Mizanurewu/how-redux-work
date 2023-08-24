6a9ec93593b4f7426d5814641128d831fe22661f
Mizanur Rahman <mizanurewu@gmail.com>
2023-08-24 17:56:45 +0600

    2.4 connect-API

---
+++ src/redux/counter/action.js
@@ -0,0 +1,53 @@
+import { INCREMENT, DECREMENT } from "./actionType"
+export const increment=(value)=>{
+    return{
+        type:INCREMENT,
+        payload:value
+    }
+}
+
+export const decrement=(value)=>{
+    return{
+        type: DECREMENT,
+        payload:value
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { DECREMENT, INCREMENT } from "./actionType"
+
+// export const increment =(value)=>{
+//     return{
+//         type: INCREMENT,
+//         payload: value,
+
+//     }
+// }
+
+// export const decrement =(value)=>{
+//     return{
+//         type: DECREMENT,
+//         payload: value,
+
+//     }
+// }
