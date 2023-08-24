aa4d1eb730bccbd6a21771b808443906e04be02d
Mizanur Rahman <mizanurewu@gmail.com>
2023-08-24 18:10:37 +0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Router'
+
+function App() {
+ 
+  return (
+    <>
+      <div >
+        <RouterProvider router={router}></RouterProvider>
+
+      </div>
+    </>
+  )
+}
+
+export default App
